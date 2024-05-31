227046e7610bd02d1cd61029ca1f9e209a222648
Mansh-977 <codingbymanish@gmail.com>
2024-05-31 21:51:53 +0530

    find the maximum number in array

--- dsa/Min_in_array.cpp
+++ dsa/Min_in_array.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
     int arr[5];
-    int ans=INT_MIN;
+    int ans=INT_MAX;
     for(int i=0;i<5;i++)
     {
         cout<<"Enter the number:";
         cin>>arr[i];
-        if(arr[i]>ans)
+        if(arr[i]<ans)
         ans=arr[i];
     }
     cout<<"MAX number="<<ans;
