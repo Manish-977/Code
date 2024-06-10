c89042682ca4dd1062ae28534e4da653420a5bd0
Mansh-977 <codingbymanish@gmail.com>
2024-06-10 20:53:10 +0530

    updata

---
+++ java/multy.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class multy{
+    public static void main (String a[])
+{
+    int a,b;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the nuber : =");
+    a=sc.nextInt();
+    System.out.println("Enter the number := ");
+    b=sc.nextInt();
+    System.out.println("Multyplay = "+a+"*"+b+"="+(a*b));
+}
+}
