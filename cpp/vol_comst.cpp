6343877614535149cf00d6fe131ac577a8a959a7
Mansh-977 <codingbymanish@gmail.com>
2024-06-11 21:10:59 +0530

    Check Whether a Character is a Vowel or Consonant

---
+++ cpp/vol_comst.cpp
@@ -0,0 +1,24 @@
+// C++ program to check if a given
+// character is vowel or consonant.
+#include <iostream>
+using namespace std;
+
+// Function to check whether a
+// character is vowel or not
+void vowelOrConsonant(char x)
+{
+	if (x == 'a' || x == 'e' || x == 'i' || x == 'o'
+		|| x == 'u' || x == 'A' || x == 'E' || x == 'I'
+		|| x == 'O' || x == 'U')
+		cout << "Vowel" << endl;
+	else
+		cout << "Consonant" << endl;
+}
+
+// Driver code
+int main()
+{
+	vowelOrConsonant('c');
+	vowelOrConsonant('E');
+	return 0;
+}
