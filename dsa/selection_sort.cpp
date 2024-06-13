689870b5e218bb61163153b6b5c30e537c3d4c2c
Mansh-977 <codingbymanish@gmail.com>
2024-06-13 12:41:05 +0530

    sorting the array

--- dsa/selection_sort.cpp
+++ dsa/selection_sort.cpp
@@ -3,21 +3,23 @@ using namespace std;
 int main()
 {
     
-    int arr[6],i;
-    for(i=0;i<6;i++){
+    int arr[1000],n,i;
+    cout<<"Enter the size of array :";
+    cin>>n; 
+    for(i=0;i<n;i++){
     cout<<"Enter the number =";
     cin>>arr[i];
     }
-    for(i=0;i<5;i++){
+    for(i=0;i<n-1;i++){
          int index=i;
-         for(int j=i+1;j<6;j++)
+         for(int j=i+1;j<n;j++)
          {
             if(arr[j]<arr[index])
             index=j;
          }
          swap(arr[i],arr[index]);
     }
-         for(int i=0;i<6;i++)
+         for(int i=0;i<n;i++)
          {
             cout<<arr[i]<<" ";
         }
