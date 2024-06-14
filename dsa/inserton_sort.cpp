a6ca4a876dca6d34ba745e50158fc55aba8e1294
Mansh-977 <codingbymanish@gmail.com>
2024-06-14 12:10:49 +0530

    Insertion sort int array

--- dsa/inserton_sort.cpp
+++ dsa/inserton_sort.cpp
@@ -5,11 +5,13 @@ int main()
     int arr[1000],i,n,j; 
     cout<<"Enter the array size  = ";
     cin>>n;
+    //input the array
     for(i=0;i<n;i++)
     {
         cout<<"Enter the number = ";
         cin>>arr[i];
     }
+    //insertion sort prog
     for(i=1;i<n;i++)
     {
         for(j=i;j>0;j--)
@@ -20,6 +22,7 @@ int main()
         break;
         }
     }
+    //output the array 
     for(i=0;i<n;i++)
     {
         cout<<arr[i]<<" ";
