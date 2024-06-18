4c7ca5e54e923368199ebc473a102e653ed71598
Mansh-977 <codingbymanish@gmail.com>
2024-06-18 20:37:55 +0530

    How to Access Array Elements

---
+++ cpp/Access_array.cpp
@@ -0,0 +1,21 @@
+//  C++ Program to Illustrate How to Access Array Elements
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[3];
+
+    // Inserting elements in an array
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+
+    // Accessing and printing elements of the array
+    cout << "arr[0]: " << arr[0] << endl;
+    cout << "arr[1]: " << arr[1] << endl;
+    cout << "arr[2]: " << arr[2] << endl;
+
+    return 0;
+}
