2140bafec572aa1fed2264cc7abd44bef37d7e63
Mansh-977 <codingbymanish@gmail.com>
2024-07-02 12:45:11 +0530

    Program to Display Multiplication Table Up to a Given Range

---
+++ cpp/multiplication.cpp
@@ -0,0 +1,19 @@
+// C++ program to print table
+// over a range
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+	// Change here to change
+	// input number
+	int n = 8;
+
+	// Change here to change result
+	int range = 11;
+	for (int i = 1; i <= range; ++i)
+		cout << n << " * " << i << " = " << n * i << endl;
+
+	return 0;
+}
