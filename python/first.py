1d787979dd523e7dc9154b0104b15fa8176ebbf1
Manish Kumar <manishkumar@Manishs-MacBook-Air.local>
2024-08-12 11:24:26 +0530

    updata

--- python/first.py
+++
@@ -1,5 +0,0 @@
-#name=input("enter a name=");
-#print(name);
-age=int(input("enter a age ="))
-print(age)
-print(748)
--- python/practice.py
+++
@@ -1,8 +0,0 @@
-#write a prog input user's first name & print its length.
-name=input("enter your name = ")
-print("lenth of your name is",len(name))
-
-
-#writr a prog fing the occurrence of $ in a string.
-str ="HI ,$Iam the $ symbol $999.99"
-print(str.count("$"))
