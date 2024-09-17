d2c62fbf0bd8cdc98169a878ea7c623543f0c9a9
Manish-977 <codingbymanish@gmail.com>
2024-09-17 21:14:31 +0530

    javascript

---
+++ java_script/2datatype.js
@@ -0,0 +1,5 @@
+"use strict ";//treat all js code as newer version
+console.log(3+4)
+let age =98
+let name="manish"
+console.table([age,name])
--- java_script/const.js
+++
@@ -1,8 +0,0 @@
-const student= {
-    fullName: "manish kumar",
-    age:24,
-    cgpa : 8.9,
-};
-
-student["age"]=student["age"]+1;
-console.log(student["age"]);
