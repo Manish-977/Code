df53cbc5b53a8f8edadafae81e3399046cfd3b93
Manish-977 <codingbymanish@gmail.com>
2024-09-18 20:57:32 +0530

    updata

---
+++ java_script/3conversionopretion.js
@@ -0,0 +1,13 @@
+let score =33
+let stringNumber = String(score)
+//console.log(typeof stringNumber)  
+
+let value =3
+let negValue =-value
+//console.log(negValue)
+
+
+let str1 ="hello"
+let str2 = " Manish"
+let str3=str1+str2
+ console.log(str3)
---
+++ java_script/datatypes.js
@@ -0,0 +1,21 @@
+// primitive
+
+// 7 types : string,Number,Boolearn,null,undefined,Symbol,BigInt
+const score = 100 
+const scoreValue =100.3
+
+const isLoggedIn = false
+ const outsideTemp =null
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+ //console.log (id ===anotherId)
+
+// Reference (non primitive)
+//Array,Object,Functions
+const heros = ["shaktiman","naagraj","doga"]
+let myObj ={
+    name:"Manish",
+    age:22,
+}
+console.log(myObj)
