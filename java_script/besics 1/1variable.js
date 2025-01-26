4c64b46dc2b345e8efb59f9f4371112c3bea83a3
manish-977 <codingbymanish@gmail.com>
2025-01-27 00:22:25 +0530

    update

--- java_script/besics 1/1variable.js
+++
@@ -1,8 +0,0 @@
-const accountId = 144338
-let accountEmail = "codingbymanish@gmail.com"
-var accountPassword = "1234"
-accountCity = "patna"
-//accountId = 1 // not allowed
-
-console.log(accountId);
-console.table([accountEmail,accountId,accountPassword,accountCity])
